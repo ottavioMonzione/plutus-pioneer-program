678c65f8d07c91623ae75e2beb6d5216fbc8c385
ottavioMonzione <ottavio.monzione@gmail.com>
2021-04-17 01:14:17 +0200

    Introduced support for devcontainer VS function and resolved the howeworks for week2

--- code/week02/src/Week02/IsDataOttavio.hs
+++ code/week02/src/Week02/IsDataOttavio.hs
@@ -8,43 +8,49 @@
 {-# LANGUAGE TypeFamilies        #-}
 {-# LANGUAGE TypeOperators       #-}
 
-module Week02.Homework1 where
 
-import           Control.Monad        hiding (fmap)
-import           Data.Map             as Map
-import           Data.Text            (Text)
-import           Data.Void            (Void)
-import           Plutus.Contract      hiding (when)
-import           PlutusTx             (Data (..))
+module Week02.IsDataOttavio where
+
+import           Control.Monad       hiding (fmap)
+import           Data.Map            as Map
+import           Data.Text           (Text)
+import           Data.Void           (Void)
+import           Plutus.Contract     hiding (when)
+import           PlutusTx            (Data (..))
 import qualified PlutusTx
-import           PlutusTx.Prelude     hiding (Semigroup(..), unless)
-import           Ledger               hiding (singleton)
-import           Ledger.Constraints   as Constraints
-import qualified Ledger.Scripts       as Scripts
-import qualified Ledger.Typed.Scripts as Scripts
-import           Ledger.Ada           as Ada
-import           Playground.Contract  (printJson, printSchemas, ensureKnownCurrencies, stage)
-import           Playground.TH        (mkKnownCurrencies, mkSchemaDefinitions)
-import           Playground.Types     (KnownCurrency (..))
-import           Prelude              (Semigroup (..))
-import           Text.Printf          (printf)
+import           PlutusTx.Prelude    hiding (Semigroup(..), unless)
+import           Ledger              hiding (singleton)
+import           Ledger.Constraints  as Constraints
+import qualified Ledger.Scripts      as Scripts
+import qualified Ledger.Typed.Scripts      as Scripts
+import           Ledger.Ada          as Ada
+import           Playground.Contract (printJson, printSchemas, ensureKnownCurrencies, stage)
+import           Playground.TH       (mkKnownCurrencies, mkSchemaDefinitions)
+import           Playground.Types    (KnownCurrency (..))
+import           Prelude             (Semigroup (..))
+import           Text.Printf         (printf)
+
+newtype MySillyRedeemer = MySillyRedeemer Integer
+    deriving Show
+
+PlutusTx.unstableMakeIsData ''MySillyRedeemer
+
 
 {-# INLINABLE mkValidator #-}
--- This should validate if and only if the two Booleans in the redeemer are equal!
-mkValidator :: () -> (Bool, Bool) -> ValidatorCtx -> Bool
-mkValidator () ( firstBool, secondBool ) _ = traceIfFalse "they do not match"  $ firstBool == secondBool
+mkValidator :: () -> MySillyRedeemer -> ValidatorCtx -> Bool
+mkValidator () ( MySillyRedeemer r ) _ = traceIfFalse "wrong redeemer" $ r == 42
 
 data Typed
 instance Scripts.ScriptType Typed where
     type instance DatumType Typed = ()
-    type instance RedeemerType Typed = (Bool, Bool)
+    type instance RedeemerType Typed = MySillyRedeemer
 
-inst :: Scripts.ScriptInstance Typed
+inst ::  Scripts.ScriptInstance Typed
 inst = Scripts.validator @Typed
     $$(PlutusTx.compile [|| mkValidator ||])
     $$(PlutusTx.compile [|| wrap ||])
     where
-        wrap = Scripts.wrapValidator @() @(Bool, Bool)
+        wrap = Scripts.wrapValidator @() @MySillyRedeemer
 
 validator :: Validator
 validator = Scripts.validatorScript inst
@@ -58,7 +64,7 @@ scrAddress = ScriptAddress valHash
 type GiftSchema =
     BlockchainActions
         .\/ Endpoint "give" Integer
-        .\/ Endpoint "grab" (Bool, Bool)
+        .\/ Endpoint "grab" Integer
 
 give :: (HasBlockchainActions s, AsContractError e) => Integer -> Contract w s e ()
 give amount = do
@@ -67,14 +73,14 @@ give amount = do
     void $ awaitTxConfirmed $ txId ledgerTx
     logInfo @String $ printf "made a gift of %d lovelace" amount
 
-grab :: forall w s e. (HasBlockchainActions s, AsContractError e) => (Bool, Bool) -> Contract w s e ()
-grab bs = do
+grab :: forall w s e. (HasBlockchainActions s, AsContractError e) => Integer -> Contract w s e ()
+grab r = do
     utxos <- utxoAt scrAddress
     let orefs   = fst <$> Map.toList utxos
         lookups = Constraints.unspentOutputs utxos      <>
                   Constraints.otherScript validator
         tx :: TxConstraints Void Void
-        tx      = mconcat [mustSpendScriptOutput oref $ Redeemer $ PlutusTx.toData bs | oref <- orefs]
+        tx      = mconcat [mustSpendScriptOutput oref $ Redeemer $ PlutusTx.toData $ MySillyRedeemer r | oref <- orefs]
     ledgerTx <- submitTxConstraintsWith @Void lookups tx
     void $ awaitTxConfirmed $ txId ledgerTx
     logInfo @String $ "collected gifts"
@@ -83,7 +89,7 @@ endpoints :: Contract () GiftSchema Text ()
 endpoints = (give' `select` grab') >> endpoints
   where
     give' = endpoint @"give" >>= give
-    grab' = endpoint @"grab" >>= grab
+    grab' = endpoint @"grab" >>=  grab
 
 mkSchemaDefinitions ''GiftSchema
 
